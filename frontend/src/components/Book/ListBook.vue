0d2c80264c8c5d36adf3daa0999d92a1aa7e4bab
Rodrigo Barbosa <vanderdigo837@outlook.com>
2019-04-27 14:42:57 -0300

    inicializando front end

---
+++ frontend/src/components/Book/ListBook.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+
+      <div class="container">
+          <div class="row">
+              <div class="col text-left">
+                  <h2>Lista de livros</h2>
+
+              </div>
+          </div>
+      </div>
+
+
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+import ListBook from '@/components/Book/ListBook'
+
+
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +13,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/books',
+      name: 'ListBook',
+      component: ListBook
     }
-  ]
+  ],
+  mode: 'history'
 })
